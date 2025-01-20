6a5e645977f3fe2e3a915d37545b63d5d4095da3
Jackson452261 <a411758@gmail.com>
2025-01-20 23:16:29 +0800

    更新title為adidas

--- src/components/HomeProducts.jsx
+++ src/components/HomeProducts.jsx
@@ -34,7 +34,7 @@ const HomeProduct = () => {
       <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
         <div className="flex justify-between items-center">
           <h2 className="text-2xl font-bold tracking-tight text-gray-900">
-            最新產品
+            Adidas最新產品
           </h2>
         </div>
 
