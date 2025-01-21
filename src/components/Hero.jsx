b74e8408b1ce3e66f9c2e4d1a17a74d145354172
Jackson452261 <a411758@gmail.com>
2025-01-22 00:06:32 +0800

    更改中文

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -26,11 +26,10 @@ const Hero = () => {
         {/* 文字部分 */}
         <div className="mb-6 flex w-full flex-col justify-center sm:mb-12 lg:mb-0 lg:w-1/3 lg:pb-24 lg:pt-48">
           <h1 className="mb-4 text-4xl font-bold text-black sm:text-5xl md:mb-8 md:text-6xl">
-            Top Fashion for a top price!
+            Adidas
           </h1>
           <p className="max-w-md leading-relaxed text-gray-500 xl:text-lg">
-            We sell only the most exclusive and high-quality products for you.
-            We are the best, so come and shop with us.
+             販售同步日本最新鞋款、服飾
           </p>
         </div>
 
